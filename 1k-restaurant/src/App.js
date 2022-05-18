9e758b40634bb21f406433ff53b609fbaec740dd
unknown <manziisrael99@gmail.com>
2022-05-18 11:26:34 +0200

    add client route

--- 1k-restaurant/src/App.js
+++ 1k-restaurant/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './pages/Home/Home';
+import Client from './pages/clients/clientsPage';
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
+          <Route path='/clients' element={<Client />} />
         </Routes>
       </Router>
     </div>
