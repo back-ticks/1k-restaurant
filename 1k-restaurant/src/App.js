acfdeaf671eeb56efdd9ee16507e2ee0753eb1a1
rexisn <ndizihiweregis06@gmail.com>
2022-05-18 21:02:35 +0200

    Made orders page but didn't dry the code

--- 1k-restaurant/src/App.js
+++ 1k-restaurant/src/App.js
@@ -1,21 +1,23 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Signup from './pages/signup/Signup';
 import Login from './pages/signup/login';
 import Home from './pages/Home/Home';
 import Client from './pages/clients/clientsPage';
 import Overview from './pages/overview/Overview';
+import Orders from './pages/orders/orders';
 function App() {
   return (
     <Router>
       <Routes>
-         <Route path="/" exact element={<Home/>} />
-        <Route path='/overview' element={<Overview/>} />
+        <Route path="/" exact element={<Home />} />
+        <Route path='/overview' element={<Overview />} />
         <Route path='/clients' element={<Client />} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path='/orders' element={<Orders />} />
       </Routes>
     </Router>
-    )
+  )
 }
 export default App;
