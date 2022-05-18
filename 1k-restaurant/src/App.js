9c0fa37d5f7fcabafc5cbb8d8491321a25ba4671
Valentin <93974733+valentindush@users.noreply.github.com>
2022-05-18 11:09:08 +0200

    added overview

--- 1k-restaurant/src/App.js
+++ 1k-restaurant/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-
+import Overview from './pages/overview/Overview';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/overview' element={<Overview/>} />
+      </Routes>
+    </Router>
   );
 }
 
