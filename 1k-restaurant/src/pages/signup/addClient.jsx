3d6f0b5dc2e60aef3d2af3ee27ecaea1f0a47489
lina-uwase <u.lina250@gmail.com>
2022-05-24 21:25:30 +0200

    new changes

--- 1k-restaurant/src/pages/signup/addClient.jsx
+++ 1k-restaurant/src/pages/signup/addClient.jsx
@@ -21,35 +21,35 @@ export default function AddClient() {
         <div className=" w-[650px] ml-auto mr-auto mt-12" >
           <div className="flex flex-col">
               <label className="font-bold">Client name</label>
-              <input type="text" placeholder="client's name" className=" border-b-2 pl-2 h-10 "></input>
+              <input type="text" placeholder="client's name" className=" border-b-2 pl-2 h-10 outline-none focus:border-b-black"></input>
           </div>
           <div className="flex flex-col mt-4">
               <label className="font-bold">Category</label>
-              <input type="text" placeholder="Choose cattegory" className=" border-b-2 pl-2 h-10 rounded-sm"></input>
+              <input type="text" placeholder="Choose cattegory" className=" border-b-2 pl-2 h-10 rounded-sm outline-none focus:border-b-black"></input>
           </div>
           <div className="flex flex-col mt-4">
               <label className="font-bold">Representantive</label>
-              <input type="text" placeholder="Names" className="border-b-2  pl-2 h-10 rounded-sm"></input>
+              <input type="text" placeholder="Names" className="border-b-2  pl-2 h-10 rounded-sm outline-none focus:border-b-black"></input>
           </div>
           <div className="flex flex-col mt-4">
               <label className="font-bold">Date of creation</label>
-              <input type="text" placeholder="Month and year" className="border-b-2  pl-2 h-10 rounded-sm"></input>
+              <input type="text" placeholder="Month and year" className="border-b-2  pl-2 h-10 rounded-sm outline-none focus:border-b-black"></input>
           </div>
           <div className="flex flex-col mt-4">
               <label className="font-bold">Address</label>
-              <input type="text" placeholder="province, district, sector, cell" className="border-b-2  pl-2 h-10 rounded-sm"></input>
+              <input type="text" placeholder="province, district, sector, cell" className="border-b-2  pl-2 h-10 rounded-sm outline-none focus:border-b-black"></input>
           </div>
           <div className="flex flex-col mt-4">
               <label className="font-bold">Email</label>
-              <input type="text" placeholder="Email" className="border-b-2  pl-2 h-10 rounded-sm"></input>
+              <input type="text" placeholder="Email" className="border-b-2  pl-2 h-10 rounded-sm outline-none focus:border-b-black"></input>
           </div>
           <div className="flex flex-col mt-4">
               <label className="font-bold">Phone</label>
-              <input type="text" placeholder="Phone" className="border-b-2 pl-2 h-10 rounded-sm active:border-blue-600 "></input>
+              <input type="text" placeholder="Phone" className="border-b-2 pl-2 h-10 rounded-sm active:border-blue-600 outline-none focus:border-b-black "></input>
           </div>
           <div className="mt-10">
             <Link to='/clients'>
-               <button type="button" className="flex  justify-center items-center text-white font-bold border-2 bg-[#46A80F] mt-10 h-10 w-36  shadow-xl" >Add client</button>
+               <button type="button" className="flex  justify-center items-center text-white font-bold border-2 bg-[#46A80F] mt-10 h-10 w-36  shadow-xl outline-none focus:border-b-black" >Add client</button>
             </Link>
           </div>
         
